d594b846be541cc12997243c60081702b0b7c8f9
peteryu <peteryu@umich.edu>
2020-10-25 17:56:15 -0400

    pushed my code in comment line 14

--- App.js
+++ App.js
@@ -11,7 +11,143 @@ import {
 import Modal from 'react-native-modalbox';
 import Slider from 'react-native-slider';
 const fetch = require('node-fetch');
+/*
+import React, { Component } from 'react';
+import MapView from 'react-native-maps';
+import { Marker } from 'react-native-maps';
+import {
+  Dimensions,
+  Button,
+  ImageStore,
+  ImageEditor,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import * as Permissions from 'expo-permissions';
+import { ScrollView } from 'react-native-gesture-handler';
+import { Linking } from 'react-native';
+import { Card, Icon } from 'react-native-elements';
+import BottomDrawer from './BottomDrawer';
+// const fetch = require('node-fetch');
 
+let model_output;
+
+class HomeScreen extends React.Component {
+  constructor(props){
+      super(props);
+      this.state = {
+        region: {
+          latitude: 0,
+          longitude: 0,
+          latitudeDelta: 1,
+          longitudeDelta: 1,
+        },
+        text: 'jjhj',
+        imgUrl: 'assets/icon.png',
+      }
+  }
+  componentDidMount() {
+    console.log('nnn');
+    const success = position => {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+      console.log(latitude, longitude);
+      this.setState({
+        region: {
+          latitude: latitude,
+          longitude: longitude,
+          latitudeDelta: 0.0122,
+          longitudeDelta: 0.0121,
+        }
+      });
+    };
+    const error = () => {
+      console.log("Unable to retrieve your location");
+    };
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
+  getInitialState() {
+    return {
+      region: {
+        latitude: 0,
+        longitude: 0,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      },
+    };
+  }
+  onRegionChange(region) {
+    console.log('ssss');
+    //this.setState({ region });
+  }
+  async findClosestRestrooms() {
+    // get closest restrooms
+    let str1 = '';
+    let res = '';
+    fetch(str1)
+      .then((response) => response.json())
+      .then((data) => {
+        res = data;
+      })
+      .then(() => {})
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  
+  render() {
+    return (
+      <View style={styles.container}>
+       <MapView style={styles.mapStyle}  region={this.state.region}
+      onRegionChange={this.onRegionChange}
+    />
+    <View style={styles.bottomBox}>
+        
+          <Text>Using geolocation JavaScript API in React</Text>
+          <Image
+            source={{ uri: this.state.imgUrl }}
+            style={{ width: 300, height: 250 }}
+          />
+          <Button
+            title="Find Closest Restrooms"
+            style={{ flex: 1 }}
+            onPress={() => {
+              // do stuff here
+            }}
+          />
+        </View>
+    </View>
+    );
+  }
+}
+
+// const RootStack = createStackNavigator({
+//   Home: HomeScreen,
+//   Details: DetailsScreen,
+// });
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+  bottomBox: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+*/
 // let model_output;
 
 var screen = Dimensions.get('window');
