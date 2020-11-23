2f806011930edd48388be6eb21759fdad244cc5a
William Chen <wxllchxn@umich.edu>
2020-11-22 22:31:18 -0500

    modal changes

--- modal.js
+++ modal.js
@@ -6,6 +6,7 @@ import {
   Dimensions,
   Image,
   View,
+  TouchableOpacity,
 } from 'react-native'
 import Modal from 'react-native-modalbox';
 
@@ -33,12 +34,28 @@ export default class RestroomModal extends Component {
     this.refs.modal.open()
   }
 
+  // TODO render navigation with pressedMarker with given index
+
   render() {
     return (
-      <Modal style={[styles.modal]} position={"bottom"} ref={"modal"} swipeArea={20}>
-        <ScrollView style={{width: screen.width, paddingLeft: 10}}>
+      <Modal style={styles.modal} position={"bottom"} ref={"modal"} swipeArea={20}>
+        <ScrollView style={styles.scroll}>
           <Text adjustsFontSizeToFit numberOfLines={1} style={styles.title}>{this.state.name}</Text>
-          <Image style={styles.image} source={{uri: this.state.image}} />
+          <View style={{flexDirection:"row"}}>
+            <Image style={styles.image} source={{uri: this.state.image}} />
+            <View style={styles.right}>
+              <Text adjustsFontSizeToFit numberOfLines={1} style={styles.text}>Rating: </Text>
+              <Text adjustsFontSizeToFit numberOfLines={1} style={styles.text}>Amenities: </Text>
+              <Text adjustsFontSizeToFit numberOfLines={1} style={styles.text}>Reviews</Text>
+            </View>
+          </View>
+          <TouchableOpacity
+            style={styles.appButtonContainer}
+            onPress={() => {
+              console.log('navigate')
+            }}>
+            <Text style={styles.appButtonText}>Navigate to this restroom</Text>
+          </TouchableOpacity>
         </ScrollView>
       </Modal>
     );
@@ -49,7 +66,12 @@ const styles = StyleSheet.create({
   modal: {
     justifyContent: 'center',
     alignItems: 'center',
-    height: 400,
+    height: 270,
+    borderRadius: 10,
+  },
+  scroll: {
+    width: screen.width,
+    paddingLeft: 10,
   },
   title: {
     color: "black",
@@ -60,10 +82,32 @@ const styles = StyleSheet.create({
   },
   text: {
     color: "black",
-    fontSize: 22,
+    fontSize: 18,
   },
   image: {
-    height: 128,
-    width: 128,
+    height: 135,
+    flex: 1,
+  },
+  right: {
+    flex:2,
+    flexDirection: 'column',
+    marginLeft: 10,
+  },
+  appButtonContainer: {
+    elevation: 8,
+    backgroundColor: "#147EFB",
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 12,
+    left: 0, 
+    right: 0,
+    marginTop: 10,
+    marginLeft:10,
+    marginRight:10,
+  },
+  appButtonText: {
+    fontSize: 18,
+    color: "#fff",
+    alignSelf: "center",
   }
 });
